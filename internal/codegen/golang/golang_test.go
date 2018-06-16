773bb8810ab563580e873fe33cc0d6caa68ead4d
Daisuke Maki <lestrrat+github@gmail.com>
2018-06-16 19:47:33 +0900

    duh

--- internal/codegen/golang/golang_test.go
+++ internal/codegen/golang/golang_test.go
@@ -3,7 +3,7 @@ package golang_test
 import (
 	"testing"
 
-	"github.com/lestrrat-go/openapi/internal/codegen"
+	"github.com/lestrrat-go/openapi/internal/codegen/golang"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -17,7 +17,7 @@ func TestExportedName(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.Expected, func(t *testing.T) {
-			if !assert.Equal(t, test.Expected, codegen.ExportedName(test.Input), `names should match`) {
+			if !assert.Equal(t, test.Expected, golang.ExportedName(test.Input), `names should match`) {
 				return
 			}
 		})
